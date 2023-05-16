e090ad8a7614b418d1262951961a8bb77b95a4e2
Alec Hannaford <alec322@gmail.com>
2023-05-15 19:41:49 -0700

    moment tz implemented instead of date constructor. running from 7-8pm

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const PORT = process.env.PORT || 4001
 const cron = require('node-cron')
 const moment = require('moment-timezone');
 const LA_TIMEZONE = 'America/Los_Angeles';
-const now = new Date()
 
-cron.schedule("*/04 * 8-12 * * *" , () => {
-    console.log('running tast every 4 seconds from 8-noon')
-    console.log('current date: ', now.toLocaleString('en-us', {timezone: LA_TIMEZONE}))
+cron.schedule("*/04 * 19-20 * * *" , () => {
+    const now = moment.tz(LA_TIMEZONE)
+    console.log('running task every 4 seconds from 7-8')
+    console.log('current date: ', now.toLocaleString())
 }, {timezone: LA_TIMEZONE});
 
 
