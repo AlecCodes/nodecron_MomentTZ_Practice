abf092ad86c0f7fa38ade7c4bfaa6579d5209103
Alec Hannaford <alec322@gmail.com>
2023-05-16 17:08:26 -0700

    running task every minute till 5pm

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const cron = require('node-cron')
 const moment = require('moment-timezone');
 const LA_TIMEZONE = 'America/Los_Angeles';
 
-cron.schedule("0 8-16 * * *" , () => {
+cron.schedule("*/1 8-17 * * *" , () => {
     const now = moment.tz(LA_TIMEZONE)
-    console.log('running task every hour from 8-4pm')
+    console.log('running task every minute from 8-5pm')
     console.log('current date: ', now.toLocaleString())
 }, {timezone: LA_TIMEZONE});
 
